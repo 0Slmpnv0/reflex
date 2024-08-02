1b49c70875a9fff7c9a125862b94998cd02299b8
Urii Alexeev <slmpnv.code.mail@gmail.com>
2024-08-02 21:34:29 +0300

    Write main page

---
+++ frontend/src/components/Nav.tsx
@@ -0,0 +1,18 @@
+export default function Nav() {
+    return (
+      <div className="nav">
+        <a href="" className="nav-item">
+          calendar
+        </a>
+        <a
+          href="another link"
+          className="nav-item"
+        >
+          change <br /> form
+        </a>
+        <a href="new link" className="nav-item">
+          How was <br />your day?
+        </a>
+      </div>
+    );
+}
