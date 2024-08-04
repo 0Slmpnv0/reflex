3dcf0407020d96f822d969c49869ab6fdf485d99
0Slmpnv0 <142727420+0Slmpnv0@users.noreply.github.com>
2024-08-04 20:51:50 +0300

    Dev (#2)
    * Write log page styles and structure. Start writing form logic
    
    * Finish log in page. Delete useless button classes, and package. This code extremely needs to be refactored....

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+
+
+interface Props {
+    children: ReactNode;
+    onClick: () => void;
+    status: 'inactive' | 'active' 
+}
+
+
+
+export default function Button({ children, onClick, status }: Props) {
+    const styles = {
+        inactive: 'button-inactive',
+        active: 'button-active',
+        submitted: 'button-submitted'
+    }
+
+    return (
+        <button 
+        type="submit"
+        onClick={status === 'inactive' ? () => {} : onClick}
+        className={styles[status]} 
+        disabled={status === 'inactive'}
+        >{children}</button>
+    )
+}
